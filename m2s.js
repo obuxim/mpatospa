98f3af8fbd81f23e56051406789f59c62bfb2e2c
Zubair Hasan Zubo <obuxim@gmail.com>
2019-01-07 04:10:57 +0600

    Version alpha-0.02
    -bug fixed
    -performance improved
    
    -need to handle errors better
    -needs code commenting

--- m2s.js
+++ m2s.js
@@ -1,54 +1,35 @@
-/*
-	Name: mpatospa
-	Extended Name:	Multi Page App to Single Page App
-	Author: Zubair Hasan
-	Author URL: https://zubairhasan.pro
-	Author Company: Octoriz
-	Copyright: https://octoriz.com
-	Version: alpha-0.01
-*/
-
-
-//First fetch all the links and store it to global variable.
-const linkElements = document.getElementsByTagName('a');
-console.log('I got the link elements');
-//Get all the links into the global variable.
-const links = new Array(linkElements.length);
-console.log('I created the links variable');
-for(i = 0; i < linkElements.length; i++){
-    links[i] = linkElements[i].getAttribute('href');
-    console.log('I stored ' + links[i] + ' to the links variable');
-}
-
-//Now that all global variables are set we can start some working.
-
-//First let's build the ajax function.
 function loadPage(url){
     let xhttp = new XMLHttpRequest();
-    console.log('I created a new XMLHttpRequest');
     xhttp.onreadystatechange = function(){
-        console.log('onreadystatechange is here');
         if(this.readyState == 4 && this.status == 200){
-            console.log('response procesed and status ok');
-            document.getElementsByTagName('body')[0].innerHTML = this.responseText;
-            console.log('I changed the body from ' + url);
+            document.documentElement.innerHTML = this.responseText;
             singly();
-            console.log('I ran singly again so it works');
         }
-    }
-    xhttp.open('GET', url, true);
+    };
+    xhttp.open('GET',url,true);
     xhttp.send();
 }
 
-//Let's work with singly now.
 function singly(){
-    //Uh oh the loop needs to be ran again.
-    for(i = 0; i < linkElements.length; i++){
-        link = links[i];
-        console.log('I got '+link);
-        linkElements[i].removeAttribute('href');
-        linkElements[i].setAttribute('onclick', 'loadPage("'+link+'");');
-        console.log('I sat click listener for '+link);
+    let linkElements = [];
+    let finalLinkElements = [];
+    let links = [];
+    linkElements = document.getElementsByTagName('a');
+    for(i=0;i<linkElements.length;i++){
+        let linkElement = linkElements[i];
+        let link = linkElement.getAttribute('href');
+        let isNativeLink = true;
+        if(link == '' || link.indexOf('#') == 0 || link.indexOf('http') >= 0){
+            isNativeLink = false;
+        }
+        if(isNativeLink){
+            finalLinkElements.push(linkElement);
+            links.push(link);
+            linkElement.removeAttribute('href');
+            linkElement.setAttribute('onclick', 'loadPage("'+link+'")');
+        }
     }
+    console.log(finalLinkElements);
+    console.log(links);
 }
 singly();
